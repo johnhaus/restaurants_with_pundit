7cabd94398fded29b3dba7f0872a03ea0ef94ad4
johnhaus <john.haus@outlook.com>
2018-12-28 00:34:33 -0300

    finished setting up devise

--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -5,7 +5,30 @@ class RestaurantPolicy < ApplicationPolicy
     end
   end
 
+  # uncomment the code below if decision is made to prevent
+  # users from creating new restaurants
+
+  # def new?
+  #   false
+  # end
+
   def create?
     return true
+  end
+
+  def edit?
+    user_is_owner?
+  end
+
+  def update?
+    user_is_owner?
+  end
+
+  def destroy?
+    user_is_owner?
+  end
+
+  def user_is_owner?
+    record.user == user
   end
 end
